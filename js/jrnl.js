3d4383f3f66729409e7e54fe06e74322389d205c
Manuel Ebert <manuel.ebert@upf.edu>
2012-04-16 15:09:50 +0200

    Updated Readme from master

--- js/jrnl.js
+++ js/jrnl.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
         var block = $("<div class='block'></div>")        
         var title = $(this).clone()
         title.attr("id", slugify(title))
-        var content = $(this).nextUntil('h2')
+        var content = $(this).nextUntil('h2, h1')
         // if (content.length == 0) {
         //     content = $(this).nextUntil()
         // }
@@ -39,6 +39,7 @@ $(document).ready(function() {
         var navitem = $("<li class='nav-header'><a href='#"+slugify(title)+"'>"+title.text()+"</a></li>");
         $(".nav").append(navitem)
         block.find('h3').each(function() {
+            console.log($(this))
             $(this).attr('id', slugify(this));
             var navitem = $("<li><a href='#"+slugify(this)+"'>"+$(this).text().replace(/:+$/g,'')+"</a></li>");
             $(".nav").append(navitem)
