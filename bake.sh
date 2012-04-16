912f219b4479f7aae882c9febd3e89e39c89b977
Manuel Ebert <manuel.ebert@upf.edu>
2012-04-16 14:52:42 +0200

    Includes Changelog

--- bake.sh
+++ bake.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 git checkout master README.md
-markdown2 README.md > tmp
-cat templates/header.html tmp templates/footer.html > index.html
-rm tmp
+git checkout master CHANGELOG.md
+markdown2 README.md > tmp_readme
+markdown2 CHANGELOG.md > tmp_log
+cat templates/header.html tmp_readme tmp_log templates/footer.html > index.html
+rm tmp_readme tmp_log
 git add index.html
 git commit -m "Updated Readme from master"
 git push -u origin gh-pages
