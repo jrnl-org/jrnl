24423670bcbd4d7381a32912aad33e10882ebe12
Manuel Ebert <manuel.ebert@upf.edu>
2012-04-16 13:08:27 +0200

    bake now pushes to github

--- bake.sh
+++ bake.sh
@@ -3,3 +3,6 @@ git checkout master README.md
 markdown2 README.md > tmp
 cat templates/header.html tmp templates/footer.html > index.html
 rm tmp
+git add index.html
+git commit -m "Updated Readme from master"
+git push -u origin gh-pages
