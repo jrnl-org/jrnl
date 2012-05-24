7b0829121fda70f6bdfdd900b4e52b8a79149870
Manuel Ebert <manuel.ebert@upf.edu>
2012-05-24 17:04:10 +0200

    Error message when journal is corrupted, closes #22

--- jrnl/Journal.py
+++ jrnl/Journal.py
@@ -50,7 +50,11 @@ class Journal:
         if not cipher:
             return ""
         crypto = AES.new(self.key, AES.MODE_CBC, cipher[:16])
-        plain = crypto.decrypt(cipher[16:])
+        try:
+            plain = crypto.decrypt(cipher[16:])
+        except ValueError:
+            print("ERROR: Your journal file seems to be corrupted. You do have a backup, don't you?")
+            sys.exit(-1)
         if plain[-1] != " ": # Journals are always padded
             return None
         else:
