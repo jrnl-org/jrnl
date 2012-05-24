87c22899c667ac81f9628dd7079a530cb9b6e465
Manuel Ebert <manuel.ebert@upf.edu>
2012-05-24 18:45:26 +0200

    Changes [Fixed] and [Improved] Strings to tags

--- bake.sh
+++ bake.sh
@@ -3,6 +3,9 @@ git checkout master README.md
 git checkout master CHANGELOG.md
 markdown2 README.md > tmp_readme
 markdown2 CHANGELOG.md > tmp_log
+sed -i 's/<li>\[/<li class="badge">\[/g' tmp_log
+sed -i 's/\[Fixed\]/<span class="change-fixed">Fixed<\/span>/g' tmp_log
+sed -i 's/\[Improved\]/<span class="change-improved">Improved<\/span>/g' tmp_log
 cat templates/header.html tmp_readme tmp_log templates/footer.html > index.html
 rm tmp_readme tmp_log
 git add index.html
