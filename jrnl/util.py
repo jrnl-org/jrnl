6fbd4e7d7eeed3cf6a48868390513e2d6d3ebc24
Manuel Ebert <manuel@1450.me>
2013-07-15 13:32:23 +0200

    Workaround for get_localzone on OS X

--- jrnl/util.py
+++ jrnl/util.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import sys
+import os
+from tzlocal import get_localzone
 
 def py23_input(msg):
     if sys.version_info[0] == 3:
@@ -8,3 +10,12 @@ def py23_input(msg):
         except SyntaxError: return ""
     else:
         return raw_input(msg)
+
+def get_local_timezone():
+    """Returns the Olson identifier of the local timezone.
+    In a happy world, tzlocal.get_localzone would do this, but there's a bug on OS X
+    that prevents that right now: https://github.com/regebro/tzlocal/issues/6"""
+    if "darwin" in sys.platform:
+        return os.popen("systemsetup -gettimezone").read().replace("Time Zone: ", "").strip()
+    else:
+        return str(get_localzone())
