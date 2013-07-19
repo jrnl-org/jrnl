6abd7d5b68163c9070067f90eaa9d96f5bfe8d55
Manuel Ebert <manuel@1450.me>
2013-07-19 12:44:39 +0200

    Mock stdin

--- jrnl/jrnl.py
+++ jrnl/jrnl.py
@@ -149,8 +149,6 @@ def cli(manual_args=None):
     mode_compose, mode_export = guess_mode(args, config)
 
     # open journal file or folder
-
-
     if os.path.isdir(config['journal']) and ( config['journal'].endswith(".dayone") or \
         config['journal'].endswith(".dayone/")):
         journal = Journal.DayOne(**config)
