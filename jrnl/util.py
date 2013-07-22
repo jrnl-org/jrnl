a84713e99ac65dbc938ab3b3074a32d99c2e7e52
Manuel Ebert <manuel@1450.me>
2013-07-22 20:08:41 +0200

    Allows getpass to get bypassed by reading from stdin

--- jrnl/util.py
+++ jrnl/util.py
@@ -3,24 +3,35 @@
 import sys
 import os
 from tzlocal import get_localzone
+import getpass as gp
+
 PY3 = sys.version_info[0] == 3
 PY2 = sys.version_info[0] == 2
+STDIN = sys.stdin
+STDERR = sys.stderr
+STDOUT = sys.stdout
+TEST = False
+__cached_tz = None
+
+def getpass(prompt):
+    if not TEST:
+        return gp.getpass(prompt)
+    else:
+        return py23_input(prompt)
+
 
 def u(s):
     """Mock unicode function for python 2 and 3 compatibility."""
     return s if PY3 else unicode(s, "unicode_escape")
 
-STDIN = sys.stdin
-STDERR = sys.stderr
-STDOUT = sys.stdout
-__cached_tz = None
-
 def prompt(msg):
     """Prints a message to the std err stream defined in util."""
-    print(msg, file=STDERR)
+    if not msg.endswith("\n"):
+        msg += "\n"
+    STDERR.write(u(msg))
 
 def py23_input(msg):
-    STDOUT.write(msg)
+    STDERR.write(u(msg))
     return STDIN.readline().strip()
 
 def get_local_timezone():
