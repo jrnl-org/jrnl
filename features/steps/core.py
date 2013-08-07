ff0b85e28b2506245b24417f0c924038a0c1a6cb
Manuel Ebert <manuel@1450.me>
2013-08-06 18:10:42 -0700

    Better tests for exporting to json

--- features/steps/core.py
+++ features/steps/core.py
@@ -63,6 +63,33 @@ def run(context, command):
 def no_error(context):
     assert context.failed is False
 
+@then('the output should be parsable as json')
+def check_output_json(context):
+    out = context.stdout_capture.getvalue()
+    assert json.loads(out)
+
+@then('"{field}" in the json output should have {number:d} elements')
+@then('"{field}" in the json output should have 1 element')
+def check_output_field(context, field, number=1):
+    out = context.stdout_capture.getvalue()
+    out_json = json.loads(out)
+    assert field in out_json
+    assert len(out_json[field]) == number
+
+@then('"{field}" in the json output should not contain "{key}"')
+def check_output_field_not_key(context, field, key):
+    out = context.stdout_capture.getvalue()
+    out_json = json.loads(out)
+    assert field in out_json
+    assert key not in out_json[field]
+
+@then('"{field}" in the json output should contain "{key}"')
+def check_output_field_key(context, field, key):
+    out = context.stdout_capture.getvalue()
+    out_json = json.loads(out)
+    assert field in out_json
+    assert key in out_json[field]
+
 @then('the output should be')
 def check_output(context):
     text = context.text.strip().splitlines()
