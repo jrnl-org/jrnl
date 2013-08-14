3549eff21520ff677becf784e4374c3c1aa515d5
Manuel Ebert <manuel@1450.me>
2013-08-14 14:00:42 -0700

    Adds regression test for #87

--- features/steps/core.py
+++ features/steps/core.py
@@ -102,6 +102,11 @@ def check_output_inline(context, text):
     out = context.stdout_capture.getvalue()
     assert text in out
 
+@then('the output should not contain "{text}"')
+def check_output_not_inline(context, text):
+    out = context.stdout_capture.getvalue()
+    assert text not in out
+
 @then('we should see the message "{text}"')
 def check_message(context, text):
     out = context.messages.getvalue()
