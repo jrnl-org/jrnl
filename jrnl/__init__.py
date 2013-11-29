48e312849710de3ca952e2f670f98a5926d998e6
Manuel Ebert <manuel@1450.me>
2013-11-29 11:26:02 -0800

    Updated docs from master

--- jrnl/__init__.py
+++ jrnl/__init__.py
@@ -7,11 +7,11 @@ jrnl is a simple journal application for your command line.
 """
 
 __title__ = 'jrnl'
-__version__ = '1.6.5'
+__version__ = '1.6.6'
 __author__ = 'Manuel Ebert'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2013 Manuel Ebert'
 
 from . import Journal
-from . import jrnl
-from .jrnl import cli
+from . import cli
+from .cli import run
