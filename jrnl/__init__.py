411b9eb8449f6bfc96b1e466c9b6b8ae64246447
Manuel Ebert <manuel@1450.me>
2014-09-02 13:27:16 -0700

    Version bump

--- jrnl/__init__.py
+++ jrnl/__init__.py
@@ -8,7 +8,7 @@ jrnl is a simple journal application for your command line.
 from __future__ import absolute_import
 
 __title__ = 'jrnl'
-__version__ = '1.9.4'
+__version__ = '1.9.5'
 __author__ = 'Manuel Ebert'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2013 - 2014 Manuel Ebert'
