e0177f4f0bff1d7876de8599783ceab479a081fb
MinchinWeb <w_minchin@hotmail.com>
2015-05-05 11:58:15 -0600

    Fixes #348
    Also fixes #322 on windows.

--- jrnl/util.py
+++ jrnl/util.py
@@ -122,7 +122,7 @@ def get_text_from_editor(config, template=""):
     with codecs.open(tmpfile, 'w', "utf-8") as f:
         if template:
             f.write(template)
-    subprocess.call(shlex.split(config['editor']) + [tmpfile])
+    subprocess.call(shlex.split(config['editor'], posix="win" not in sys.platform) + [tmpfile])
     with codecs.open(tmpfile, "r", "utf-8") as f:
         raw = f.read()
     os.close(filehandle)
