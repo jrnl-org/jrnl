1ea5255aa0419ca3c1a02ec81528b8e904115d66
Philip Douglass <philip@philipdouglass.com>
2015-08-28 12:08:25 -0400

    Made line wrapping more clever about whitespace

--- jrnl/Entry.py
+++ jrnl/Entry.py
@@ -48,11 +48,10 @@ class Entry:
         if not short and self.journal.config['linewrap']:
             title = textwrap.fill(date_str + " " + self.title, self.journal.config['linewrap'])
             body = "\n".join([
-                    textwrap.fill((line + " ") if (len(line) == 0) else line,
+                    textwrap.fill(line,
                         self.journal.config['linewrap'],
                         initial_indent="| ",
-                        subsequent_indent="| ",
-                        drop_whitespace=False)
+                        subsequent_indent="| ") or "|"
                     for line in self.body.rstrip(" \n").splitlines()
                 ])
         else:
