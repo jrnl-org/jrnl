12647bc0bda185845eb72aa02c26b155807704ac
jnozsc <jnozsc@gmail.com>
2017-09-16 13:49:35 -0700

    rollback time related packages

--- setup.py
+++ setup.py
@@ -141,10 +141,10 @@ setup(
     description = "A command line journal application that stores your journal in a plain text file",
     packages = ['jrnl'],
     install_requires = [
-        "parsedatetime>=2.4",
-        "pytz>=2017.2",
+        "parsedatetime>=1.2",
+        "pytz>=2013b",
         "six>=1.10.0",
-        "tzlocal>=1.4",
+        "tzlocal>=1.1",
         "keyring>=3.3",
         "keyrings.alt>=1.3",
     ] + [p for p, cond in conditional_dependencies.items() if cond],
