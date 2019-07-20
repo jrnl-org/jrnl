b1aaccdb0d801a76642e045a47abee7fcda6ad90
Micah Jerome Ellison <git@micahjero.me>
2019-07-20 15:17:53 -0700

    Updating Python version and jrnl version for v2.0-rc3

--- setup.py
+++ setup.py
@@ -105,10 +105,7 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Office/Business :: News/Diary',
         'Topic :: Text Processing'
     ],
