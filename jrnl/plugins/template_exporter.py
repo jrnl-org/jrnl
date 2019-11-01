dace253513dc0c750c6f5a223302262ca4f9b26e
Peter Schmidbauer <peter.schmidb@gmail.com>
2019-11-01 13:16:10 +0100

    fix broken pyupgrade fstring

--- jrnl/plugins/template_exporter.py
+++ jrnl/plugins/template_exporter.py
@@ -34,7 +34,7 @@ def __exporter_from_file(template_file):
     """Create a template class from a file"""
     name = os.path.basename(template_file).replace(".template", "")
     template = Template.from_file(template_file)
-    return type(str(f"{name.title(}Exporter")), (GenericTemplateExporter, ), {
+    return type(str(f"{name.title()}Exporter"), (GenericTemplateExporter, ), {
         "names": [name],
         "extension": template.extension,
         "template": template
