da55914ec3716a1de575f5ba9c2f8c0464ee4706
Jonathan Wren <jonathan@nowandwren.com>
2019-12-10 22:05:38 -0800

    [#766] Skip the broken test on windows for now

--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+import sys
 
 
 def before_feature(context, feature):
@@ -9,6 +10,9 @@ def before_feature(context, feature):
         feature.skip("Marked with @skip")
         return
 
+    if "skip_win" in feature.tags and "win32" in sys.platform:
+        feature.skip("Skipping on Windows")
+        return
 
 def before_scenario(context, scenario):
     """Before each scenario, backup all config and journal test data."""
@@ -36,6 +40,9 @@ def before_scenario(context, scenario):
         scenario.skip("Marked with @skip")
         return
 
+    if "skip_win" in scenario.effective_tags and "win32" in sys.platform:
+        scenario.skip("Skipping on Windows")
+        return
 
 def after_scenario(context, scenario):
     """After each scenario, restore all test data and remove working_dirs."""
