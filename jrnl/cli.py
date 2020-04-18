e57a51ba25fe22933e5e40c980d02889dddf7b36
Jonathan Wren <jonathan@nowandwren.com>
2020-04-18 13:31:25 -0700

    Upgrade license to GPLv3 (#918)
    The MIT license is a bit outdated, and doesn't provide the protections
    we'd like in a modern open-source application.
    
    Co-authored-by: Micah Jerome Ellison <micah.jerome.ellison@gmail.com>

--- jrnl/cli.py
+++ jrnl/cli.py
@@ -3,7 +3,20 @@
 """
     jrnl
 
-    license: MIT, see LICENSE for more details.
+    license: GPLv3, see LICENSE.md for more details.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 from .Journal import PlainJournal, open_journal
