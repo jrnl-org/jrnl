d74dd3bb6f4be3bd5d26ed93a3d0d48053a06565
Eshan Ramesh <esrh@netc.eu>
2020-05-26 09:54:34 -0400

    fix keyring problems

--- jrnl/util.py
+++ jrnl/util.py
@@ -61,8 +61,7 @@ def create_password(
     if yesno("Do you want to store the password in your keychain?", default=True):
         set_keychain(journal_name, pw)
     else:
-        set_keychain(journal_name, None)
-
+        pass
     return pw
 
 
@@ -108,7 +107,10 @@ def set_keychain(journal_name, password):
         except keyring.errors.PasswordDeleteError:
             pass
     else:
-        keyring.set_password("jrnl", journal_name, password)
+        try:
+            keyring.set_password("jrnl", journal_name, password)
+        except keyring.errors.NoKeyringError:
+            print("Keyring backend not found. Please install one of the supported backends by visiting: https://pypi.org/project/keyring/")
 
 
 def yesno(prompt, default=True):
