fd25c959a57c4c2449e4d433119b37e97e211f81
Micah Jerome Ellison <micah.jerome.ellison@gmail.com>
2020-07-11 10:49:02 -0700

    Make error message more descriptive and friendly

--- jrnl/cli.py
+++ jrnl/cli.py
@@ -30,7 +30,7 @@ import jrnl
 
 from . import install, plugins, util
 from .Journal import PlainJournal, open_journal
-from .util import ERROR_COLOR, RESET_COLOR, UserAbort
+from .util import WARNING_COLOR, ERROR_COLOR, RESET_COLOR, UserAbort
 
 log = logging.getLogger(__name__)
 logging.getLogger("keyring.backend").setLevel(logging.ERROR)
@@ -318,7 +318,11 @@ def run(manual_args=None):
         "3.7"
     ):
         print(
-            "WARNING: Python versions below 3.7 are not supported by jrnl\n",
+            f"""{WARNING_COLOR}
+WARNING: Python versions below 3.7 will no longer be supported as of jrnl v2.5
+(the next release). You are currently on Python {platform.python_version()}. Please update to
+Python 3.7 (or higher) soon.
+{RESET_COLOR}""",
             file=sys.stderr,
         )
 
