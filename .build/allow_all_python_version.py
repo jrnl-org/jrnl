040007f55445bced0df3ef7e852e89a2e688f6a3
Micah Jerome Ellison <micah.jerome.ellison@gmail.com>
2020-08-08 12:46:23 -0700

    Format changes and changing before_install to script for 3.9 builds

--- .build/allow_all_python_version.py
+++ .build/allow_all_python_version.py
@@ -2,7 +2,7 @@ import toml
 
 pyproject = toml.load("pyproject.toml")
 
-pyproject['tool']['poetry']['dependencies']['python'] = "*"
+pyproject["tool"]["poetry"]["dependencies"]["python"] = "*"
 
 with open("pyproject.toml", "w") as toml_file:
-	toml.dump(pyproject, toml_file)
+    toml.dump(pyproject, toml_file)
