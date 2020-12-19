c4e6a694167b371ac79eaa252531a49fddcb4a4c
Jonathan Wren <jonathan@nowandwren.com>
2020-12-19 14:27:29 -0800

    testing ci

--- jrnl/cli.py
+++ jrnl/cli.py
@@ -16,6 +16,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+# This is a test
 import logging
 import sys
 
