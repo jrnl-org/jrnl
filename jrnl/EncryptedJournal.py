3b83c549ffb028e7d1c7257b73cdaecdb8ad0630
karimpwnz <karim@karimrahal.com>
2021-01-02 04:46:21 +0200

    Deal with locked exceptions elsewhere too

--- jrnl/EncryptedJournal.py
+++ jrnl/EncryptedJournal.py
@@ -186,12 +186,14 @@ def set_keychain(journal_name, password):
     if password is None:
         try:
             keyring.delete_password("jrnl", journal_name)
-        except keyring.errors.PasswordDeleteError:
+        except (keyring.errors.KeyringLocked, keyring.errors.PasswordDeleteError):
             pass
     else:
         try:
             keyring.set_password("jrnl", journal_name, password)
-        except keyring.errors.NoKeyringError:
+        except (keyring.errors.KeyringLocked):
+            pass
+        except (keyring.errors.NoKeyringError):
             print(
                 "Keyring backend not found. Please install one of the supported backends by visiting: https://pypi.org/project/keyring/",
                 file=sys.stderr,
