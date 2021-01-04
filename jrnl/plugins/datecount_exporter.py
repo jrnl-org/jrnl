37fc6c7465d39ab449f00370c2989d8bb38a6d18
karimpwnz <karim@karimrahal.com>
2021-01-04 02:52:55 +0200

    Update datecount format

--- jrnl/plugins/datecount_exporter.py
+++ jrnl/plugins/datecount_exporter.py
@@ -23,5 +23,5 @@ class DatecountExporter(TextExporter):
         date_counts = get_date_counts(journal)
         if not date_counts:
             return "[No dates found in journal.]"
-        result = "\n".join(f"{date} {count}" for date, count in date_counts.items())
+        result = "\n".join(f"{date}, {count}" for date, count in date_counts.items())
         return result
