65e48c3c47b02a245cfa00c958b29cfc6e345a6f
karimpwnz <karim@karimrahal.com>
2021-01-04 02:41:28 +0200

    Fix plugin import

--- jrnl/plugins/__init__.py
+++ jrnl/plugins/__init__.py
@@ -3,7 +3,6 @@
 # Copyright (C) 2012-2021 jrnl contributors
 # License: https://www.gnu.org/licenses/gpl-3.0.html
 
-from jrnl.plugins.datecount_exporter import DatecountExporter
 from .fancy_exporter import FancyExporter
 from .jrnl_importer import JRNLImporter
 from .json_exporter import JSONExporter
