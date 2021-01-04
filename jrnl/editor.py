ccf7d22a2d716eb9eb20adb65c0708cba77ad4c6
karimpwnz <karim@karimrahal.com>
2021-01-04 04:38:41 +0200

    Fix formatting

--- jrnl/editor.py
+++ jrnl/editor.py
@@ -11,6 +11,7 @@ from .color import ERROR_COLOR
 from .color import RESET_COLOR
 from .os_compat import on_windows
 
+
 def get_text_from_editor(config, template=""):
     suffix = ".jrnl"
     if config["template"]:
@@ -27,8 +28,8 @@ def get_text_from_editor(config, template=""):
         # https://stackoverflow.com/questions/33560364/python-windows-parsing-command-lines-with-shlex
         # https://bugs.python.org/issue1724822
         if on_windows:
-            parsed_editor_path = config["editor"] + tmpfile 
-        else: 
+            parsed_editor_path = config["editor"] + tmpfile
+        else:
             parsed_editor_path = shlex.split(config["editor"]) + [tmpfile]
         subprocess.call(parsed_editor_path)
     except Exception as e:
