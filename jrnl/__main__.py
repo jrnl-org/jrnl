355bd3f293b390500a21fdd55baad48f070d296e
MinchinWeb <w_minchin@hotmail.com>
2021-06-26 21:26:52 -0600

    Restore `__main__.py` functionality
    Allows you to access jrnl by running `python -m jrnl`

---
+++ jrnl/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Copyright (C) 2012-2021 jrnl contributors
+# License: https://www.gnu.org/licenses/gpl-3.0.html
+
+import sys
+
+from .cli import cli
+
+if __name__ == "__main__":
+    sys.exit(cli())
