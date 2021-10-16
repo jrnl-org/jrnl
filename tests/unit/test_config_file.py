31354ba6697ab0221cf47502f382cb5dff750a50
Micah Jerome Ellison <micah.jerome.ellison@gmail.com>
2021-10-16 15:19:18 -0700

    Move config file unit test to unit test dir and use pytext path fixture instead of current directory to find test data

--- tests/unit/test_config_file.py
+++ tests/unit/test_config_file.py
@@ -1,16 +1,21 @@
 import pytest
+import os
 
 from jrnl.install import find_alt_config
 
 
-def test_find_alt_config():
-    work_config_path = "features/data/configs/work-config.yaml"
+def test_find_alt_config(request):
+    work_config_path = os.path.join(
+        request.fspath.dirname, "..", "data", "configs", "basic_onefile.yaml"
+    )
     found_alt_config = find_alt_config(work_config_path)
     assert found_alt_config == work_config_path
 
 
-def test_find_alt_config_not_exist():
-    bad_config_path = "features/data/configs/not-existing-config.yaml"
+def test_find_alt_config_not_exist(request):
+    bad_config_path = os.path.join(
+        request.fspath.dirname, "..", "data", "configs", "not-existing-config.yaml"
+    )
     with pytest.raises(SystemExit) as ex:
         found_alt_config = find_alt_config(bad_config_path)
         assert found_alt_config is not None
