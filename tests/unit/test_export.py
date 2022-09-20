53e7be72defd139b6bec84c429e42c0052c80622
outa <outa@users.noreply.github.com>
2022-09-20 21:00:52 +0200

    Call `export_journal` before opening file handle

--- tests/unit/test_export.py
+++ tests/unit/test_export.py
@@ -1,15 +1,29 @@
 # Copyright © 2012-2022 jrnl contributors
 # License: https://www.gnu.org/licenses/gpl-3.0.html
 
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
 import pytest
+from ruamel.yaml import YAML
 
 from jrnl.exception import JrnlException
+from jrnl.Journal import PlainJournal
 from jrnl.plugins.fancy_exporter import check_provided_linewrap_viability
+from jrnl.plugins.text_exporter import TextExporter
 
 
 @pytest.fixture()
 def datestr():
     yield "2020-10-20 16:59"
+
+
+@pytest.fixture()
+def simple_journal():
+    with open("tests/data/configs/simple.yaml") as f:
+        yaml = YAML(typ="safe")
+        config = yaml.load(f)
+    return PlainJournal("simple_journal", **config).open()
 
 
 def build_card_header(datestr):
@@ -28,3 +42,12 @@ class TestFancy:
 
         with pytest.raises(JrnlException):
             check_provided_linewrap_viability(total_linewrap, [content], journal)
+
+
+class TestYaml:
+    def test_export_to_nonexisting_folder(self, simple_journal):
+        with TemporaryDirectory() as tmpdir:
+            p = Path(tmpdir / "non_existing_folder")
+            with pytest.raises(JrnlException):
+                TextExporter.write_file(simple_journal, p)
+            assert not p.exists()
