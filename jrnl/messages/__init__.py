16f6bca2fe8ba2bd8724a167f9e0ed660c8e7441
Jonathan Wren <jonathan@nowandwren.com>
2022-10-30 15:35:24 -0700

    fix linting issues in current codebase

--- jrnl/messages/__init__.py
+++ jrnl/messages/__init__.py
@@ -1,6 +1,10 @@
 # Copyright © 2012-2022 jrnl contributors
 # License: https://www.gnu.org/licenses/gpl-3.0.html
 
-from jrnl.messages.Message import Message  # noqa: F401
-from jrnl.messages.MsgStyle import MsgStyle  # noqa: F401
-from jrnl.messages.MsgText import MsgText  # noqa: F401
+from jrnl.messages import Message
+from jrnl.messages import MsgStyle
+from jrnl.messages import MsgText
+
+Message = Message.Message
+MsgStyle = MsgStyle.MsgStyle
+MsgText = MsgText.MsgText
