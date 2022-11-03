c1eb0c54a35049b46e0f718cea6cda823852fb93
Jonathan Wren <jonathan@nowandwren.com>
2022-11-03 07:18:35 -0700

    Add `type-checking` plugin to linting checks (#1629)
    * add type-checking plugin for flakeheaven
    
    * update lock file
    
    * fix type-checking issues in current codebase
    
    * run linters

--- jrnl/exception.py
+++ jrnl/exception.py
@@ -1,14 +1,18 @@
 # Copyright © 2012-2022 jrnl contributors
 # License: https://www.gnu.org/licenses/gpl-3.0.html
 
-from jrnl.messages import Message
+from typing import TYPE_CHECKING
+
 from jrnl.output import print_msg
+
+if TYPE_CHECKING:
+    from jrnl.messages import Message
 
 
 class JrnlException(Exception):
     """Common exceptions raised by jrnl."""
 
-    def __init__(self, *messages: Message):
+    def __init__(self, *messages: "Message"):
         self.messages = messages
 
     def print(self) -> None:
--- jrnl/messages/Message.py
+++ jrnl/messages/Message.py
@@ -1,14 +1,17 @@
 # Copyright © 2012-2022 jrnl contributors
 # License: https://www.gnu.org/licenses/gpl-3.0.html
 
+from typing import TYPE_CHECKING
 from typing import Mapping
 from typing import NamedTuple
 
 from jrnl.messages.MsgStyle import MsgStyle
-from jrnl.messages.MsgText import MsgText
+
+if TYPE_CHECKING:
+    from jrnl.messages.MsgText import MsgText
 
 
 class Message(NamedTuple):
-    text: MsgText
+    text: "MsgText"
     style: MsgStyle = MsgStyle.NORMAL
     params: Mapping = {}
