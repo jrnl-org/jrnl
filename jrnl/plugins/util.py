86886394c033f14ef4e0a1e63987e09bef831699
Jonathan Wren <jonathan@nowandwren.com>
2024-02-26 20:07:32 -0800

    run formatter

--- jrnl/plugins/util.py
+++ jrnl/plugins/util.py
@@ -8,8 +8,9 @@ if TYPE_CHECKING:
     from jrnl.journals import Journal
 
 
-"""https://stackoverflow.com/a/74873621/8740440"""
 class NestedDict(dict):
+    """https://stackoverflow.com/a/74873621/8740440"""
+
     def __missing__(self, x):
         self[x] = NestedDict()
         return self[x]
